dbaece157ec13e3a432a763b71c8f7b3410ba290
Josef Martinec <josef.martinec@keboola.com>
2022-01-25 12:55:04 +0100

    PS-2836 [shared-bundle] Update to DBAL v3.3, rewrite the retry to use DBAL middleware instead of custom Connection class

---
+++ README.md
@@ -0,0 +1,6 @@
+# Job Queue Shared Bundle
+
+## Environment
+```
+TEST_DATABASE_URL - database connection URL (mysql://user:secret@localhost/mydb)
+```
--- src/DependencyInjection/Compiler/ConfigureDbalRetryProxyPass.php
+++ src/DependencyInjection/Compiler/ConfigureDbalRetryProxyPass.php
@@ -4,30 +4,95 @@ declare(strict_types=1);
 
 namespace Keboola\JobQueueSharedBundle\DependencyInjection\Compiler;
 
-use Keboola\JobQueueSharedBundle\Database\ConnectionWithRetry;
+use Keboola\JobQueueSharedBundle\Database\Retry\Middleware;
+use Retry\Policy\SimpleRetryPolicy;
+use Retry\RetryProxy;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 
 class ConfigureDbalRetryProxyPass implements CompilerPassInterface
 {
-    private const RETRY_PROXY_SERVICE = 'doctrine.dbal.retry_proxy';
+    private const CONFIG_OPTION_RETRIES = 'x_connect_retries';
+    private const SET_MIDDLEWARES_METHOD = 'setMiddlewares';
 
     public function process(ContainerBuilder $container): void
     {
-        if (!$container->hasDefinition(self::RETRY_PROXY_SERVICE)) {
-            return;
-        }
-
         $connections = (array) $container->getParameter('doctrine.connections');
         foreach ($connections as $serviceName) {
-            $connectionDefinition = $container->getDefinition($serviceName);
+            $connectionParams = $this->getConnectionParams($container, $serviceName);
+            $retries = (int) ($connectionParams['driverOptions'][self::CONFIG_OPTION_RETRIES] ?? 0);
 
-            if (!is_a((string) $connectionDefinition->getClass(), ConnectionWithRetry::class, true)) {
+            if ($retries === 0) {
                 continue;
             }
 
-            $connectionDefinition->addMethodCall('setRetryProxy', [new Reference(self::RETRY_PROXY_SERVICE)]);
+            $retryMiddlewareReference = $this->createRetryMiddleware($container, $serviceName, $retries);
+            $configurationDefinition = $this->getConfigurationServiceDefinition($container, $serviceName);
+
+            $middlewares = $this->getConfiguredMiddlewares($configurationDefinition);
+            $middlewares = [$retryMiddlewareReference, ...$middlewares];
+
+            $configurationDefinition->removeMethodCall(self::SET_MIDDLEWARES_METHOD);
+            $configurationDefinition->addMethodCall(self::SET_MIDDLEWARES_METHOD, [$middlewares]);
         }
+    }
+
+    private function getConnectionParams(ContainerBuilder $container, string $connectionServiceName): array
+    {
+        $definition = $container->getDefinition($connectionServiceName);
+        $params = $definition->getArgument(0);
+        assert(is_array($params));
+
+        return $params;
+    }
+
+    private function getConfigurationServiceDefinition(
+        ContainerBuilder $container,
+        string $connectionServiceName
+    ): Definition {
+        $connectionDefinition = $container->getDefinition($connectionServiceName);
+
+        $configurationReference = $connectionDefinition->getArgument(1);
+        assert($configurationReference instanceof Reference);
+
+        return $container->getDefinition((string) $configurationReference);
+    }
+
+    private function getConfiguredMiddlewares(Definition $configurationDefinition): array
+    {
+        foreach (array_reverse($configurationDefinition->getMethodCalls()) as [$method, $arguments]) {
+            if ($method !== self::SET_MIDDLEWARES_METHOD) {
+                continue;
+            }
+
+            return $arguments[0];
+        }
+
+        return [];
+    }
+
+    private function createRetryMiddleware(
+        ContainerBuilder $containerBuilder,
+        string $namePrefix,
+        int $retries
+    ): Reference {
+        $retryPolicyServiceName = sprintf('%s.retry_middleware.retry_proxy.retry_policy', $namePrefix);
+        $containerBuilder->setDefinition($retryPolicyServiceName, new Definition(SimpleRetryPolicy::class, [
+            $retries,
+        ]));
+
+        $retryProxyServiceName = sprintf('%s.retry_middleware.retry_proxy', $namePrefix);
+        $containerBuilder->setDefinition($retryProxyServiceName, new Definition(RetryProxy::class, [
+            new Reference($retryPolicyServiceName),
+        ]));
+
+        $middlewareServiceName = sprintf('%s.retry_middleware', $namePrefix);
+        $containerBuilder->setDefinition($middlewareServiceName, new Definition(Middleware::class, [
+            new Reference($retryProxyServiceName),
+        ]));
+
+        return new Reference($middlewareServiceName);
     }
 }
